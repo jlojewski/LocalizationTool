1f87b7e9aa5af9b52dc77970e393ed4db0e76234
jlojewski <jedrzejlojewski@gmail.com>
2023-04-04 16:34:20 +0200

    mid-progress commit @implementing checksums

--- src/main/java/loc/TranslationFileContent.java
+++ src/main/java/loc/TranslationFileContent.java
@@ -12,5 +12,28 @@ public class TranslationFileContent {
     private String checksumKey;
     private List<TranslationEntry> contentList;
 
+    public String getChecksumKey() {
+        return checksumKey;
+    }
+
+    public void setChecksumKey(String checksumKey) {
+        this.checksumKey = checksumKey;
+    }
+
+    public List<TranslationEntry> getContentList() {
+        return contentList;
+    }
+
+    public void setContentList(List<TranslationEntry> contentList) {
+        this.contentList = contentList;
+    }
+
+    @JsonCreator
+    public TranslationFileContent(@JsonProperty("checksumKey") String checksumKey, @JsonProperty("contentList") List<TranslationEntry> contentList) {
+        this.checksumKey = checksumKey;
+        this.contentList = contentList;
+    }
+
+
 
 }
